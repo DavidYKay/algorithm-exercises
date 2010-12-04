977d4261ffa9a53ff0daad519c064de87920033f
David Young-Chan Kay <davidykay@gmail.com>
2010-12-03 21:20:41 -0500

    Stubbed makeSubsets

--- chap1/ex27.rb
+++ chap1/ex27.rb
@@ -1,7 +1,16 @@
 #!/usr/bin/env ruby
 
 module Lotto
-
+  def makeSubsets(k, set)
+    #subsets = [ ]
+    #0.upto(set.length - 1) do
+    #  1.upto(k) do
+    #    subset = ___
+    #    subsets.push(subset)
+    #  end
+    #end
+    #subsets
+  end
 end
 
 #Lotto
